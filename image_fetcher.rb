be5b7fb0488571f206aba41b989855ea15e5664d
rachellogie <rachel.logie1@gmail.com>
2014-05-05 09:21:34 -0600

    All tests pass

--- image_fetcher.rb
+++ image_fetcher.rb
@@ -7,15 +7,19 @@ class ImageFetcher
   end
 
   def count_children
+    @data["data"]["children"].length
   end
 
   def first_child
+    @data["data"]["children"].first["data"]
   end
 
   def all_images
+    @data["data"]["children"].map { |child| child["data"]["url"]}
   end
 
   # fetch only the images that begin with `http://i.imgur.com`
   def only_imgur_images
+    @data["data"]["children"].map { |child| child["data"]["url"]}.select{ |image| image =~ /i\.imgur/}
   end
 end
